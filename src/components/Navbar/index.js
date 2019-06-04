96cab2ed61d3433df7fb51a7909b10ce94cca3db
Jessie Sidhu <jsidhu22@gmail.com>
2019-06-04 15:11:22 -0700

    navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='row justify-content-md-center' style={{fontFamily: 'Questrial'}}>
+            <div className='col-md-10 display-2 border-bottom border-light text-light'>
+                Smart Kara
+            </div> 
+        </div>
+    )
+}
+
+export default Navbar;
